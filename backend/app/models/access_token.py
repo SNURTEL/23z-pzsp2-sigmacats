35d2799aa4fd29234c041ac1f05455baabc4c0aa
Mewash1 <mizak.milosz@gmail.com>
2023-12-02 20:47:48 +0100

    evening out with main

--- backend/app/models/access_token.py
+++
@@ -1,6 +0,0 @@
-from app.base import Base
-from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTableUUID
-
-
-class AccessToken(SQLAlchemyBaseAccessTokenTableUUID, Base):
-    pass
